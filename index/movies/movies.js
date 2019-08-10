e3ea9e5adcef87d98b45091984cce922b5d89f00
root-lucas <945832032@qq.com>
2019-08-10 20:36:51 +0800

    movies-API-data

--- index/movies/movies.js
+++ index/movies/movies.js
@@ -1,1 +1,70 @@
-Page({})
+var app = getApp();
+// var util = require('../../utils/util.js')
+
+Page({
+  data: {
+    inTheaters: {},   //也可以将{}写为单引号或者双引号''
+    comingSoon: {},
+    top250: {},
+    },
+  onLoad:function(event){
+      //设置API地址
+      var inTheatersUrl = app.globalData.doubanBase +
+        "/v2/movie/in_theaters" + "?start=0&count=3&apikey=0df993c66c0c636e29ecbb5344252a4a&start=0";
+      var comingSoonUrl = app.globalData.doubanBase +
+        "/v2/movie/coming_soon" + "?start=0&count=3&apikey=0df993c66c0c636e29ecbb5344252a4a&start=0";
+      var top250Url = app.globalData.doubanBase +
+        "/v2/movie/top250" + "?start=0&count=3&apikey=0df993c66c0c636e29ecbb5344252a4a&start=0";
+      //请求获取API接口信息
+      this.getMovieListData(inTheatersUrl, "inTheaters", "正在热映");
+      this.getMovieListData(comingSoonUrl, "comingSoon", "即将上映");
+      this.getMovieListData(top250Url, "top250", "豆瓣Top250");
+  },
+
+  /*1.网络请求函数*/
+  getMovieListData: function (url, settedKey, categoryTitle) {
+    var that = this;
+    wx.request({
+      url: url,
+      data: '',
+      method: 'GET',
+      //必须要设置"content-type":"json",不然会报错 400 (Bad Request)
+      header: {
+        "Content-Type": "json"
+      },
+      success: function (res) { 
+        console.log(categoryTitle,res)
+        that.processDoubanData(res.data, settedKey, categoryTitle)
+        },
+      fail: function (error) { console.log("请求出错:", error) },
+      complete: function () { },
+    })
+  },
+  //2.对豆瓣API数据进行处理
+  processDoubanData: function (moviesDouban, settedKey, categoryTitle) {
+      var movies = [];  //设置空数组记录循环处理完的数据
+      for (var idx in moviesDouban.subjects) {
+        var subject = moviesDouban.subjects[idx];
+        var title = subject.title;
+        if (title.length >= 6) {
+          title = title.substring(0, 6) + "...";  //设置电影标题长度为6个子
+        }
+        // [1,1,1,1,1] [1,1,1,0,0]
+        var temp = {
+          title: title,
+          average: subject.rating.average,
+          coverageUrl: subject.images.large,
+          movieId: subject.id
+        }
+        movies.push(temp)
+      }
+      //执行到这里movies等于[{..},{..},{..}]
+      var readyData = {};
+      readyData[settedKey] = {
+        movies: movies
+      };  
+      //执行到这里readyData[settedKey]等于{movies:Array(3)}
+      //readyData则是{top250:{movies:[...],[...],[...]}},这里的top250是上面data定义的变量对象
+    this.setData(readyData); 
+  },
+})
